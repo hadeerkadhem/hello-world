00e32ccc921e9ec01d6578b42b48aa964971fadf
pochin345 <hadeerkadhem@yahoo.com>
2020-02-04 00:32:25 -0800

    print date

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,8 +1,11 @@
 package edu.pcc.cis.helloworld;
 
+import java.util.Date;
+
 public class HelloWorld{
 
     public static void main(String[] args) {
         System.out.println("Hello world");
+        system.out.println(new Date());
     }
 }
