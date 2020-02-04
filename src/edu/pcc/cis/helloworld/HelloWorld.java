1db01a20457dad1dda0fa6626ce0d2454bb6da6f
pochin345 <hadeerkadhem@yahoo.com>
2020-02-04 00:52:36 -0800

    initialize project

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -6,6 +6,7 @@ public class HelloWorld{
 
     public static void main(String[] args) {
         System.out.println("Hello world");
-        system.out.println(new Date());
+        system
+                .out.println(new Date());
     }
 }
